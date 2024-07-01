f1910843edf7164d8554de33b249cd0cecb7df9c
TaiShang <1328629765@qq.com>
2024-07-01 14:50:18 +0800

    create spring boot project

---
+++ Backend/CFSF/src/main/java/com/example/cfsf/CfsfApplication.java
@@ -0,0 +1,13 @@
+package com.example.cfsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfsfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CfsfApplication.class, args);
+    }
+
+}
